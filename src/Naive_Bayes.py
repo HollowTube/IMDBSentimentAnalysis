01ac53e62647dffb256ae9d2f23fbe786c9c6972
Tritin Truong <tritin_truong@yahoo.com>
2019-02-18 11:52:25 -0500

    Added simple documentation

--- src/Naive_Bayes.py
+++ src/Naive_Bayes.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 # Convention y = 1 means positive review
-class naiveBayes():
+class NaiveBayes:
     def __init__(self, smoothing=False):
         self.marginal_y_1 = None
         self.marginal_y_0 = None
@@ -16,14 +16,15 @@ class naiveBayes():
         num_rows = x_train.shape[0]
         num_features = x_train.shape[1]
 
+        # Getting the marginal probability of class y=1
         num_class_1 = 0
         for index, row in y_train.iterrows():
             num_class_1 += row[0]
         num_class_0 = num_rows - num_class_1
-
         self.marginal_y_1 = float(num_class_1) / (x_train.shape[0])
-        self.marginal_y_0 = 1 - self.marginal_y_1
+        self.marginal_y_0 = 1.0 - self.marginal_y_1
 
+        # Getting the conditional probabilities
         cond_y1 = np.zeros(num_features)
         cond_y0 = np.zeros(num_features)
 
@@ -37,6 +38,7 @@ class naiveBayes():
                         cond_y0[feature_index] += 1
                 feature_index += 1
 
+        # Finishing up the conditional array, and deciding the apply smoothing or not
         if self.smoothing:
             cond_y0 = cond_y0 + 1
             cond_y1 = cond_y1 + 1
@@ -50,8 +52,9 @@ class naiveBayes():
         prob_y1 = self.marginal_y_1
         prob_y0 = self.marginal_y_0
 
-        result = np.zeros((x_test.shape[0], 1))
+        result = np.zeros((x_test.shape[0]))
 
+        # Calculating probabilities for each class
         for index, row in x_test.iterrows():
             feature_index = 0
             for feature in row:
@@ -67,4 +70,4 @@ class naiveBayes():
             else:
                 result[index] = 1
 
-        return pd.DataFrame(result)
+        return result
--- src/naive_bayes_test.py
+++ src/naive_bayes_test.py
@@ -3,12 +3,14 @@ import pandas as pd
 import numpy as np
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.metrics import confusion_matrix
-from src.Naive_Bayes import naiveBayes
+from src.Naive_Bayes import NaiveBayes
 
 
 class test_naive_bayes(TestCase):
 
     def test_bernoulli_bayes(self):
+
+        # Sample training set
         data = np.array([
             [0, 1, 1],
             [0, 1, 1],
@@ -17,31 +19,36 @@ class test_naive_bayes(TestCase):
             [1, 1, 1],
             [0, 1, 0],
             [1, 0, 1]])
-        data_test = np.array([[0, 0, 0],
-                              [1, 1, 1],
-                              [1, 0, 1],
-                              [0, 1, 0],
-                              [1, 1, 0]])
+
+        # Sample test set
+        data_test = np.array([
+            [0, 0, 0],
+            [1, 1, 1],
+            [1, 0, 1],
+            [0, 1, 0],
+            [1, 1, 0]])
+
+        # Making the training set and test set for the x_set
         x_train = pd.DataFrame(data)
         x_test = pd.DataFrame(data_test)
 
+        # Making the training set for the y_set
         y_train = pd.DataFrame(np.array([[1], [1], [0], [0], [0], [0], [1]]))
         y_actual = pd.DataFrame(np.array([[0], [1], [1], [0], [0]]))
 
         # Testing it with sk-learn implementation
         bnb = BernoulliNB()
-        bnb.fit(x_train,y_train)
+        bnb.fit(x_train, y_train)
         y_pred_1 = bnb.predict(x_test)
         cm_sk = confusion_matrix(y_actual, y_pred_1)
 
-
         # Testing it on homemade implementation
-        nb = naiveBayes(smoothing=True)
+        nb = NaiveBayes(smoothing=True)
         nb.fit(x_train, y_train)
         y_pred = nb.predict(x_test)
         cm = confusion_matrix(y_actual, y_pred)
 
-        self.assertTrue(np.array_equal(cm,cm_sk))
+        self.assertTrue(np.array_equal(cm, cm_sk))
 
 
 
